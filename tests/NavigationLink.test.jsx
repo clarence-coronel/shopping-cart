9da75d285e94c4da9aa388d36d8aa0b482667da9
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-06-29 17:03:31 +0800

    feat: finish navigation test suite

--- tests/NavigationLink.test.jsx
+++ tests/NavigationLink.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import NavigationLink from "../src/component/NavigationLink";
-import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { describe, expect, it } from "vitest";
 
 describe("NavigationLink", () => {
   it("should display given text if given", () => {
@@ -13,6 +14,38 @@ describe("NavigationLink", () => {
       </BrowserRouter>
     );
 
-    // expect(screen.getByText(text)).toBeInTheDocument();
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+
+  it("should display child node instead of text if given", () => {
+    const attrText = "Home";
+    const childText = "Child Div";
+
+    render(
+      <BrowserRouter>
+        <NavigationLink to="/" text={attrText}>
+          <div>{childText}</div>
+        </NavigationLink>
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText(childText)).toBeInTheDocument();
+    expect(screen.queryByText(attrText)).not.toBeInTheDocument();
+  });
+
+  it("should point href to given route", () => {
+    const attrText = "Home";
+    const route = "/test-route";
+
+    render(
+      <BrowserRouter>
+        <NavigationLink to={route} text={attrText} />
+      </BrowserRouter>
+    );
+
+    const linkElement = screen.getByText(attrText).closest("a");
+
+    // Assert that the link element has the correct href attribute
+    expect(linkElement).toHaveAttribute("href", route);
   });
 });
