ac1d5802b72b8e077bcc468c35e8b28a477725ec
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-06-30 18:37:44 +0800

    feat: finish cart

---
+++ src/component/CartItem.jsx
@@ -0,0 +1,158 @@
+import { useContext, useEffect, useState } from "react";
+import { Cart as CartData } from "../App";
+import { parse } from "postcss";
+
+function CartItem({ product }) {
+  const [cart, setCart] = useContext(CartData);
+  const [quantity, setQuantity] = useState(
+    product.quantity ? product.quantity : 1
+  );
+  const [totalPrice, setTotalPrice] = useState(
+    parseFloat(product.quantity ? product.quantity : 1) *
+      parseFloat(product.price)
+  );
+
+  const index = cart.findIndex((item) => item.id === product.id);
+
+  useEffect(() => {
+    setTotalPrice(
+      parseFloat(product.quantity ? product.quantity : 1) *
+        parseFloat(product.price)
+    );
+  }, [cart]);
+
+  const truncate = (str = "", limit) => {
+    if (str.length <= limit) return str;
+
+    return str.substring(0, limit) + "...";
+  };
+
+  const decrease = () => {
+    if (quantity == 1) {
+      const tempCart = cart.filter((item) => item.id !== product.id);
+
+      setCart(tempCart);
+    } else {
+      const tempCart = [...cart];
+
+      tempCart[index].quantity -= 1;
+
+      setQuantity(quantity - 1);
+      setCart([...tempCart]);
+    }
+  };
+
+  const increase = () => {
+    if (quantity == 9) {
+      alert("Max Item Quantity Reached.");
+      return;
+    } else {
+      const tempCart = [...cart];
+
+      tempCart[index].quantity += 1;
+      setQuantity(quantity + 1);
+
+      setCart([...tempCart]);
+    }
+  };
+
+  //   const handleInputChange = (e) => {
+  //     const tempCart = [...cart];
+
+  //     if (isNaN(e.target.value)) {
+  //       setQuantity(1);
+  //       tempCart[index].quantity = 1;
+
+  //       setCart([...tempCart]);
+  //       return;
+  //     }
+
+  //     if (parseInt(e.target.value) > 9) {
+  //       setQuantity(9);
+  //       tempCart[index].quantity = 9;
+
+  //       setCart([...tempCart]);
+  //       return;
+  //     }
+
+  //     setQuantity(e.target.value);
+  //     tempCart[index].quantity = e.target.value;
+  //     setCart([...tempCart]);
+  //   };
+
+  //   const handleInputBlur = (e) => {
+  //     const tempCart = [...cart];
+
+  //     if (isNaN(e.target.value) || !e.target.value) {
+  //       setQuantity(1);
+  //       tempCart[index].quantity = 1;
+
+  //       setCart([...tempCart]);
+  //       return;
+  //     }
+  //   };
+
+  return (
+    <div className="flex gap-5 justify-between items-center p-5">
+      <div className="flex gap-5 items-center justify-start">
+        <img
+          className="w-20 h-20 border-2 aspect-square rounded-sm"
+          src={product.image}
+          alt=""
+        />
+        <span title={product.title}>{truncate(product.title, 40)}</span>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="p-2 flex gap-5 items-center border-purple-600">
+          {/* Decrease item quantity btn*/}
+          <button
+            onClick={() => decrease()}
+            className="text-xl font-bold rounded-md flex justify-center items-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.5rem"
+              height="1.5rem"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M18 12.998H6a1 1 0 0 1 0-2h12a1 1 0 0 1 0 2"
+              />
+            </svg>
+          </button>
+          {/* Quantity */}
+          {/* <input
+              className="focus:outline-purple-600 text-center p-2 border-2 aspect-square w-10 h-10 flex justify-center items-center rounded-sm bg-white"
+              value={quantity}
+              onChange={handleInputChange}
+              onBlur={handleInputBlur}
+            /> */}
+          <span className="focus:outline-purple-600 text-center p-2 border-2 aspect-square w-10 h-10 flex justify-center items-center rounded-sm bg-white">
+            {quantity}
+          </span>
+          {/* Increase item quantity btn */}
+          <button
+            onClick={() => increase()}
+            className="text-xl font-bold rounded-md flex justify-center items-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.5rem"
+              height="1.5rem"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1"
+              />
+            </svg>
+          </button>
+        </div>
+        <div>${totalPrice}</div>
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,46 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Cart as CartData } from "../App";
+import CartItem from "../component/CartItem";
 
 function Cart() {
-  const [cart, addToCart] = useContext(CartData);
+  const [cart, setCart] = useContext(CartData);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    let tempTotal = 0;
+
+    cart.forEach((product) => {
+      let quantity = parseInt(product.quantity ? product.quantity : 1);
+      let price = parseFloat(product.price);
+      let totalProductPrice = quantity * price;
+
+      tempTotal += totalProductPrice;
+
+      // alert("updated");
+    });
+
+    setTotal(tempTotal);
+  }, [cart]);
+
+  if (cart.length === 0)
+    return (
+      <div className="text-2xl font-semibold w-full max-w-[1400px] mx-auto p-10 flex justify-center items-center">
+        Cart is empty...
+      </div>
+    );
 
   return (
-    <div className="">
+    <div className="divide-y-2 w-full max-w-[1400px] mx-auto p-10">
       {cart.map((item) => (
-        <div key={item.id}>
-          {item.title} - {item.quantity}
-        </div>
+        <CartItem key={item.id} product={item} />
       ))}
+
+      <div className="p-5 flex justify-end items-center gap-5">
+        Total: ${total}
+        <button className="hover:bg-green-600 p-2 bg-green-500 text-white rounded-md duration-200">
+          Proceed
+        </button>
+      </div>
     </div>
   );
 }
